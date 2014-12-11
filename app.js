267add0e3af2ce5d193d8aff651aa8f488e15bbf
Dhaval Kapil <dhavalkapil@gmail.com>
2014-12-11 16:11:49 +0530

    Added logger.js for handling logs

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-
 var dns = require('./lib/dns.js');
+var args = require('./lib/args.js');
+var logger = require('./lib/logger.js');
 
-dns.startCapture();
+logger.config.log = args.params.log;
+logger.config.verbose = args.params.verbose;
+
+dns.startCapture(logger.config);
